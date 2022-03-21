c22abdfb9a6e1f1c9f4f27bb33aadfa24c2f961f
Kathryn <kpanger88@gmail.com>
2022-03-20 22:38:48 -0500

    raw documents extracted

--- hospital.py
+++ hospital.py
@@ -2,21 +2,35 @@ from os import listdir
 from os.path import isfile, join
 from document import Document
 import pandas as pd
+from xlrd import XLRDError
 
 
 class Hospital:
     def __init__(self, path: str):
         self.path = path
         # Hospital name
-        self.hospital = path.split("\\")[-1]
+        self.name = path.split("\\")[-1]
         # Dictionary of files and document objects
         self.documents = {}
         # File
         self.docNames = [f for f in listdir(path) if isfile(join(path, f))]
         for document in self.docNames:
-            try:
-                data = pd.ExcelFile(join(path, document))
-                self.documents[document] = Document(data)
-            except ValueError:
-                continue
+            filename = document.split(".")
+            extension = filename[-1]
+            correctFileExtensions = ["xls", "xlsx"]
+            if extension in correctFileExtensions:
+                try:
+                    data = pd.ExcelFile(join(path, document))
+                    self.documents[document] = Document(data)
+                except XLRDError:
+                    print(f"XLRDError: document not imported \n "
+                          f"{self.name}: {document}")
+                    continue
+                except IOError:
+                    print(f"IOError: document not imported \n "
+                          f"{self.name}: {document}")
+                except ValueError:
+                    continue
+
+
             
--- main.py
+++ main.py
@@ -3,11 +3,19 @@ from document import Document
 from hospital import Hospital
 import os
 
-folder = "../chargemaster-cdm-2021/Alameda Hospital"
-testHospital = Hospital(folder)
-docs = testHospital.documents
-one_doc = docs['106010735_CDM_All_2021.xlsx']
-sheets = (one_doc.sheets)
-ab1045 = sheets["AB 1045"]
-data = ab1045.data
-print(data)
+# folder = "../chargemaster-cdm-2021/Alameda Hospital"
+# testHospital = Hospital(folder)
+# docs = testHospital.documents
+# one_doc = docs['106010735_CDM_All_2021.xlsx']
+# sheets = (one_doc.sheets)
+# ab1045 = sheets["AB 1045"]
+# data = ab1045.data
+# print(data)
+
+directory = "../chargemaster-cdm-2021/"
+subfolders = [ f.path for f in os.scandir(directory) if f.is_dir() ]
+for folder in subfolders:
+    hospital = Hospital(folder)
+    print(hospital.name)
+    docNames = hospital.docNames
+    print(docNames)
