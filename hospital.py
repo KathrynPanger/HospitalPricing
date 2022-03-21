8b10fbe76ff0ab17b23160d2d83ced56670a0494
Kathryn <kpanger88@gmail.com>
2022-03-20 21:33:03 -0500

    fixed valuerror problem

--- hospital.py
+++ hospital.py
@@ -3,6 +3,7 @@ from os.path import isfile, join
 from document import Document
 import pandas as pd
 
+
 class Hospital:
     def __init__(self, path: str):
         self.path = path
@@ -10,8 +11,12 @@ class Hospital:
         self.hospital = path.split("\\")[-1]
         # Dictionary of files and document objects
         self.documents = {}
-        # Files
+        # File
         self.docNames = [f for f in listdir(path) if isfile(join(path, f))]
         for document in self.docNames:
-            data = pd.ExcelFile(join(path, document))
-            self.documents[document] = Document(data)
+            try:
+                data = pd.ExcelFile(join(path, document))
+                self.documents[document] = Document(data)
+            except ValueError:
+                continue
+            
