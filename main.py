a8a6281a1d814e2ee13ed22ada7cd37a3362beba
Kathryn <kpanger88@gmail.com>
2022-04-15 19:12:29 -0500

    rename 1045s

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #https://www.cms.gov/medicaremedicare-fee-service-paymentphysicianfeeschedpfs-relative-value-files/rvu21d
-from document import Document
+
 from hospital import Hospital
 from util import getBestMatch
 import os
@@ -13,9 +13,12 @@ import os
 # data = ab1045.data
 # print(data)
 
+
 directory = "../chargemaster-cdm-2021/"
 subfolders = [ f.path for f in os.scandir(directory) if f.is_dir() ]
-forms = []
+
+
+#Rename all AB 1045 forms to be called "AB1045"
 for folder in subfolders[0:10]:
     hospital = Hospital(folder)
     documents = hospital.documents
@@ -23,7 +26,23 @@ for folder in subfolders[0:10]:
         match = getBestMatch([name for name in value.sheetNames],
                              "AB 1045", 38)
         if match is not None:
-                forms.append(value.sheets[match])
+                AB1045 = value.sheets[match]
+                AB1045.name = "AB1045"
+
+#print(forms)
+
+for folder in subfolders[0:10]:
+    hospital = Hospital(folder)
+    documents = hospital.documents
+    forms = []
+    for key, value in documents.items():
+        match1 = getBestMatch([name for name in value.sheetNames],
+                             "chargemaster", 38)
+        match2 = getBestMatch([name for name in value.sheetNames],
+                              "CDM", 38)
+        if match is not None and match2 is not None:
+                CDM = value.sheets[match]
+                forms.append(CDM)
+                #AB1045.name = "AB1045"
 
 print(forms)
-
